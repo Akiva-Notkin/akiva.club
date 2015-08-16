3fefbcc3d01836dc150f67ef3e7e0f24f1701d64
Akiva-Notkin <akiva.notkin@gmail.com>
2015-08-16 11:48:55 -0700

    Testing collumns

--- public_html/index.php
+++ public_html/index.php
@@ -22,12 +22,32 @@
 
   <div class="container-fluid">
   	<div class="row">
-  		<div class="col-md-12">
-  			<img src="images/cats.jpg" class="img-responsive" alt="Responsive image">
-  			<img src="images/cats.jpg" class="img-responsive" alt="Responsive image">
-  			<img src="images/cats.jpg" class="img-responsive" alt="Responsive image">
-  		</div>
-  	</div>
+  <div class="col-md-1">.col-md-1</div>
+  <div class="col-md-1">.col-md-1</div>
+  <div class="col-md-1">.col-md-1</div>
+  <div class="col-md-1">.col-md-1</div>
+  <div class="col-md-1">.col-md-1</div>
+  <div class="col-md-1">.col-md-1</div>
+  <div class="col-md-1">.col-md-1</div>
+  <div class="col-md-1">.col-md-1</div>
+  <div class="col-md-1">.col-md-1</div>
+  <div class="col-md-1">.col-md-1</div>
+  <div class="col-md-1">.col-md-1</div>
+  <div class="col-md-1">.col-md-1</div>
+</div>
+<div class="row">
+  <div class="col-md-8">.col-md-8</div>
+  <div class="col-md-4">.col-md-4</div>
+</div>
+<div class="row">
+  <div class="col-md-4">.col-md-4</div>
+  <div class="col-md-4">.col-md-4</div>
+  <div class="col-md-4">.col-md-4</div>
+</div>
+<div class="row">
+  <div class="col-md-6">.col-md-6</div>
+  <div class="col-md-6">.col-md-6</div>
+</div>
   </div>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->

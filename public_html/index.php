679d01ab1c2a382b2bb1f754011032677d3a0b14
Akiva-Notkin <akiva.notkin@gmail.com>
2015-08-16 12:20:41 -0700

    Two images

--- public_html/index.php
+++ public_html/index.php
@@ -22,20 +22,11 @@
 
   <div class="container-fluid">
   	<div class="row">
-  <div class="col-md-1">.col-md-1</div>
-  <div class="col-md-1">.col-md-1</div>
-  <div class="col-md-1">.col-md-1</div>
-  <div class="col-md-1">.col-md-1</div>
-  <div class="col-md-1">.col-md-1</div>
-  <div class="col-md-1">.col-md-1</div>
-  <div class="col-md-1">.col-md-1</div>
-  <div class="col-md-1">.col-md-1</div>
-  <div class="col-md-1">.col-md-1</div>
-  <div class="col-md-1">.col-md-1</div>
-  <div class="col-md-1">.col-md-1</div>
-  <div class="col-md-1">To switch directories, type <kbd>cd</kbd> followed by the name of the directory.<br>
-To edit settings, press <kbd><kbd>ctrl</kbd> + <kbd>,</kbd></kbd>
-  </div>
+  		<div class="col-md-12">
+  		<img src="images/Pleiades_Spitzer_big.jpg" class="img-responsive" alt="Responsive image">
+  		<img src="images/Pleiades_Spitzer_big.jpg" class="img-responsive" alt="Responsive image">
+  		</div>
+  	</div>
   </div>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->

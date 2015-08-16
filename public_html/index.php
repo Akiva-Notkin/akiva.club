7a6b614cd3cb802a5ca2905987146f701ac3eef2
Akiva-Notkin <akiva.notkin@gmail.com>
2015-08-16 12:39:17 -0700

    Navbar

--- public_html/index.php
+++ public_html/index.php
@@ -19,8 +19,19 @@
   </head>
   <body>
     <h1>Hello, world!</h1>
-
+<nav class="navbar navbar-default">
   <div class="container-fluid">
+  	<div class="navbar-header">
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+    <div>
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Page 1</a></li>
+        <li><a href="#">Page 2</a></li>
+        <li><a href="#">Page 3</a></li>
+      </ul>
+    </div>
   	<div class="row">
   		<div class="col-md-12">
   			<div id="mycarousel" class="carousel slide" data-ride="carousel">
@@ -42,6 +53,7 @@
   		</div>
   	</div>
   </div>
+  </nav>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>

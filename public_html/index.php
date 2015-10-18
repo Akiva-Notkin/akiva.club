bc608c79b18813537993f4aba4dd07b466840f2d
akivanotkin <akiva13@gmail.com>
2015-10-17 18:31:47 -0700

    thing

--- public_html/index.php
+++ public_html/index.php
@@ -18,7 +18,7 @@
     <![endif]-->
   </head>
   <body>
-	<nav class="navbar navbar-fixed-top" background="#000000">
+	<nav class="navbar navbar-fixed-top" background="#808080">
   		<div class="container-fluid">
   			<div class="navbar-header">
       			<a class="navbar-brand" href="#">WebSiteName</a>

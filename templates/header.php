d83ff85c717cdecc1de202e396b5e2b4f064701b
akivanotkin <akiva13@gmail.com>
2015-10-18 09:32:10 -0700

    w php

--- templates/header.php
+++ templates/header.php
@@ -29,9 +29,9 @@
               }
               if($navbar != 'Page1') {
                 echo '<li class="dropdown">
-          <a href="works/endersshadow.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Works <span class="caret"></span></a>
+          <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Works <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
+            <li><a href="works/endersshadow.php">Action</a></li>
             <li><a href="#">Another action</a></li>
             <li><a href="#">Something else here</a></li>
             <li role="separator" class="divider"></li>

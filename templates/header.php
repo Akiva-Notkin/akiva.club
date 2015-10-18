6575f968745eb825a5a05cfa0aafd86ee20833c6
akivanotkin <akiva13@gmail.com>
2015-10-17 21:31:18 -0700

    with a jumbotron

--- templates/header.php
+++ templates/header.php
@@ -2,8 +2,13 @@
 
 <html>
   <head>
-    <meta name="viewport" content="width=device-width">
-    <title><?php echo htmlspecialchars($title) ?></title>
+      	<meta charset="utf-8">
+      	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+      	<meta name="viewport" content="width=device-width, initial-scale=1">
+    	<title><?php echo htmlspecialchars($title) ?></title>
+    	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+      	<script src="js/bootstrap.min.js"></script>
+      	<link href="css/bootstrap.min.css" rel="stylesheet">
   </head>
   <body>
-    <h1><?php echo htmlspecialchars($title) ?></h1>
+

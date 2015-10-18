c8377d70e432ac08b086de09a54e89081738bc30
akivanotkin <akiva13@gmail.com>
2015-10-17 22:14:18 -0700

    save

--- public_html/index.php
+++ public_html/index.php
@@ -11,15 +11,15 @@ require_once('../includes/helpers.php');
 <div class="container-fluid">
     <div class="jumbotron">
         <h1>Words create worlds.</h1>
-        <p>
-        	Language was established to explain the world around us. We wanted to communicate to others what we saw, and hear from them what they did. We began telling stories. 
-        </p>
+        <p>        </p>
   	</div>
-  	<!-- <div class="row">
+  	<div class="row">
   		<p>
-  			Language was established to explain the world around us. 
+  			Language was established to explain the world around us. While it had a very utilitarian use at first, 
+  			language began to be used for everything. Words were used to tell grandiose stories about places both real
+  			and imagined. When one person saw, or even imagined a place, everyone else was able to see it. 
   		</p>
-  	</div> -->
+  	</div>
 </div>
 
 <?php render('footer'); ?>

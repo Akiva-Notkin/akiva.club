57829c0f167e10859427979e8ff4486ffa58a905
akivanotkin <akiva13@gmail.com>
2015-12-14 14:35:41 -0800

    adding back in

--- public_html/works/endersshadow.php
+++ public_html/works/endersshadow.php
@@ -5,22 +5,3 @@ require_once('../../includes/helpers.php');
 render('worksheaderheader', array('title' => 'Ender\'s Shadow');	
 
 ?>
-
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-1"></div>
-		<div class="col-md-10">
-			<p>
-						Ender’s Shadow Series
-			Ender’s Shadow and the following books in the series focus on Bean, a short but growing kid, who helped Ender defeat an alien race known as the buggers. When he is brought down to Earth following the victory, the planet is unsure of what to do.
-
-			For the past hundred years, earth had been threatened by an alien presence. They never knew if they were going to be attacked, and were forced to band together against the common threat. But when this threat dissipated, the world changed dramatically. Hundreds, if not thousands, of battle trained children came down and were enlisted into their national armies, or occasionally kidnapped and forced to work for others.
-
-			The most apt comparison we have for a victory over a common enemy, is the defeat of the Nazi’s in World War II. When the USSR came into Berlin, the alliance between the USSR and the USA broke down. The most significant difference between the end of the bugger threat and the Nazi’s was the loss of life. Millions died in the invasion of the buggers, but they were never expected to come back alive, nor were they on earth in the past 70 years or so. After World War Two, the USSR’s population was decimated. They could not afford to fight another major war. After the buggers, almost everyone could.
-			</p>
-		</div>
-	</div>
-</div>
-
-<?php render('footer'); ?>
- 

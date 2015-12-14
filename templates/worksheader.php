0bbb6a0db748689fce2f22ae861fdb86b3473719
akivanotkin <akiva13@gmail.com>
2015-12-14 14:43:31 -0800

    step back

--- templates/worksheader.php
+++ templates/worksheader.php
@@ -8,8 +8,8 @@
       	<meta name="viewport" content="width=device-width, initial-scale=1">
     	<title><?php echo htmlspecialchars($title) ?></title>
     	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-      	<script src="js/bootstrap.min.js"></script>
-      	<link href="css/bootstrap.min.css" rel="stylesheet">
+      	<script src="../js/bootstrap.min.js"></script>
+      	<link href="../css/bootstrap.min.css" rel="stylesheet">
   </head>
   <body>
   		<body>

753ba4f3ed4a63bdd776c55dd00670733963b871
akivanotkin <akiva13@gmail.com>
2015-12-14 14:24:42 -0800

    navbar

--- templates/header.php
+++ templates/header.php
@@ -14,9 +14,6 @@
   		<body>
   	  <nav class="navbar navbar-default" background="#808080">
     		<div class="container-fluid">
-    		  <div class="navbar-header">
-        		<a class="navbar-brand" href="#">WebSiteName</a>
-      		</div>
       	  <div>
         		<ul class="nav navbar-nav">
             
@@ -27,47 +24,16 @@
               else {
                 echo '<li class="active"><a href="#">Home</a></li>';
               }
-              if($navbar != 'Page1') {
-                echo '<li class="dropdown">
-          <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Works <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="works/endersshadow.php">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li>';
-              }
-              else {
-                echo '<li class="dropdown-active">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Works <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li> 
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li>';
-              }
-              if($navbar != 'Page2') {
-                echo '<li><a href="#">Page 2</a></li>';
-              }
-              else {
-                echo '<li class="active"><a href="#">Page 2</a></li>';
-              }
-              if($navbar != 'Page3') {
-                echo '<li><a href="#">Page 3</a></li>';
-              }
-              else {
-                echo '<li class="active"><a href="#">Page 3</a></li>';
-              }
-              ?>
+              ?><li class="dropdown">
+                <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Works <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="works/endersshadow.php">Ender's Shadow</a></li>
+                  <li><a href="works/fallout.php">Fallout</a></li>
+                  <li><a href="works/bioshock.php">Something else here</a></li>
+                </ul>
+              </li>;
+              
+              
         			</ul>
      	 	  </div>
      	</nav>

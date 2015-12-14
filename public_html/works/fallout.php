c632736ff793a567b451b761d96f77c56c6f082d
akivanotkin <akiva13@gmail.com>
2015-12-14 14:58:35 -0800

    fallout

---
+++ public_html/works/fallout.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once('../../templates/worksheader.php');
+headerworks("Fallout");
+
+?>
+
+
+<div class="container-fluid">
+	<br>
+	<div class="row">
+	<div class="col-md-1"></div>
+			<img src="../images/fallout_banner.jpg">
+	</div>
+	<br><br>
+	<div class="row">
+
+		<div class="col-md-1"></div>
+		<div class="col-md-7">
+			<p style="font-size:20px">
+			A world where a nuclear apocalypse happens is a very different world. Not only do the traditional societal and governmental systems collapse, the biological nature of many species, even humans can change.
+<br><br> 
+
+In the Fallout series, you begin as a character around 200 years after a global nuclear apocalypse. From the onset you have to decide who you want to be. Is your character going to be a shoot first ask questions later type of character? Are you going to sneak around everywhere acting like a kleptomaniac? Do you want to be who you think you would be in an apocalyptic scenario or someone completely different? The type of character you choose to be changes how you see the game significantly changing the lessons you see.
+<br><br>
+Quickly you get released into an open world for you to explore. Now you can go on the main quest and explore the main story to see what happens but by running around randomly you can find things to do. You find people with stories of woe or of happiness and you get to decide if you want to help them. Who you help matters as you explore the world. The game remembers and word gets around surprisingly fast sometimes.
+<br><br>
+One of the main plot lines of the first few games is that a prewar government entity known as the Enclave attempts to destroy all “contaminated” individuals in the mainland, which encompasses almost everyone, and repopulate the US. When you are among those who are supposed to be terminated because small parts of you changed, you treat those who want to attack all slightly differently. 
+
+			</p>
+		</div>
+	</div>
+</div>
+
+
+<?php require("../../templates/footer.php"); ?> 

a6e3f28b9eca9ba146ad9c51d96b4eb52bffa7b2
akivanotkin <akiva13@gmail.com>
2015-12-14 18:41:47 -0800

    spell check

--- public_html/works/bioshock.php
+++ public_html/works/bioshock.php
@@ -18,11 +18,11 @@ headerworks("BioShock");
 		<div class="col-md-1"></div>
 		<div class="col-md-7">
 			<p style="font-size:20px">
-			When you start this game you are quickly taken to a city in the sky. You see an idealistic society where everyone is happy and comfortable with their living situation. But everybody is white. The first black person you see is tied up while others throw baseballs at him, for dating a white woman. Soon you find that people literally live below the city, working hard so those in the city can live easy. 
+			When you start this game you are quickly taken to a city in the sky. You see an idealistic society where everyone is happy and comfortable with their living situation. But everybody is white. The first black person you see is tied up while others throw baseballs at him, for dating a white woman. Soon you find that others, including the black people, live below the city, working hard so those in the city can live easy. 
 <br><br>
 These people have a floating city. They were able to transport a sizeable population into the sky where they felt they would have a more comfortable life. Can you only have this comfortable life when there are others worse off than you who keep up your standard of living? 
 <br><br>
-In the course of the story, those under the city rebel against the city, pushing the previous population out. You are never told how this new society functions, instead you question if this new society, which was created through rebellion, was a positive. The new society might easily dive back into the same mindset where a portion of the population is subservient to the rest. The story wants you to make assumptions about a world you never get to see the full verion of.
+In the course of the story, those under the city rebel against the city, pushing the previous population out. You are never told how this new society functions, instead you question if this new society, which was created through rebellion, was a positive. The new society might easily dive back into the same mindset where a portion of the population is subservient to the rest. The story wants you to make assumptions about a world you never get to see the full version of.
 
 			</p>
 		</div>
